ae5f05881f2a89f483a0983dee5dac4d9cce8361
Keril-png <kirabramofff@gmail.com>
2021-03-17 22:14:25 +0300

    4

--- api/views.py
+++ api/views.py
@@ -11,10 +11,10 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import AccessToken
 
-from .api_filters import TitleFilter, 
+from .api_filters import TitleFilter
 from .models import CustomUser, Category, Genre, Title, Review, Comment
 from .permissions import IsAdminPermission, IsAdminOrReadOnly, IsAuthorOrModeratorOrAdmin
-from .serializers import UserCreationSerializer, LoggingUserSerializer, CategorySerializer, GenreSerializer, TitleSerializer, TitleEditSerializer, ReviewSerializer, CommentSerializer
+from .serializers import CustomUserSerializer, UserCreationSerializer, LoggingUserSerializer, CategorySerializer, GenreSerializer, TitleSerializer, TitleEditSerializer, ReviewSerializer, CommentSerializer
 
 
 class SendConfirmationCodeView(APIView):
